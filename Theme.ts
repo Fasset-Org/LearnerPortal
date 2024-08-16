a23a845bc400a42fe21749385d3eac89a7213695
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-08-16 16:52:30 +0200

    Create Theme.ts

---
+++ Theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from "@rneui/themed";
+
+const themeLight = createTheme({
+  lightColors: {
+    primary: "#163683",
+    secondary: "#14a37f"
+  }
+});
+
+export default themeLight;
