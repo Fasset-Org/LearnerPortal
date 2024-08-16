f8d44184362fd287bef1c6ab3b52ba4ae6b91108
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-08-16 16:53:25 +0200

    Create learnerprogramme.tsx

---
+++ app/(tabs)/learnerprogramme.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const LearnerProgramme = () => {
+  return (
+    <View>
+      <Text>LearnerProgramme</Text>
+    </View>
+  );
+};
+
+export default LearnerProgramme;
+
+const styles = StyleSheet.create({});
