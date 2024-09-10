9d29eb24e57efa00e4c846340f4aee6abe327a17
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-09-10 10:36:34 +0200

    Create EditUserAddressOverlay.tsx

---
+++ components/EditUserAddressOverlay.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { Button, Overlay, Icon } from "@rneui/themed";
+import { View, Text, StyleSheet } from "react-native";
+import themeLight from "../Theme";
+
+type EditUserAddressOverlay = {};
+
+const EditUserAddressOverlay: React.FunctionComponent<
+  EditUserAddressOverlay
+> = () => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleOverlay = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <View>
+      <Icon
+        name="pencil-square-o"
+        size={30}
+        type="font-awesome"
+        style={styles.iconStyle}
+        color="#FFFFFF"
+        onPress={toggleOverlay}
+      />
+      <Overlay
+        isVisible={visible}
+        onBackdropPress={toggleOverlay}
+        overlayStyle={styles.fullScreenOverlay}
+      >
+        <View style={styles.overlayHeader}>
+          <Text style={styles.textPrimary}>Edit Address Information</Text>
+          <Icon
+            name="close"
+            type="material"
+            color="white"
+            size={30}
+            onPress={toggleOverlay}
+          />
+        </View>
+        <Text style={styles.textSecondary}>
+          Welcome to React Native Elements
+        </Text>
+        <Button
+          icon={
+            <Icon
+              name="wrench"
+              type="font-awesome"
+              color="white"
+              size={25}
+              iconStyle={{ marginRight: 10 }}
+            />
+          }
+          title="Start Building"
+          onPress={toggleOverlay}
+        />
+      </Overlay>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    margin: 10
+  },
+  overlayHeader: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderColor: "lightgray",
+    backgroundColor: themeLight.lightColors?.primary,
+    paddingHorizontal: 10,
+    height: 60
+  },
+  textPrimary: {
+    fontSize: 17,
+    color: "#FFFFFF"
+  },
+  textSecondary: {
+    marginBottom: 10,
+    textAlign: "center",
+    fontSize: 17
+  },
+  iconStyle: {
+    color: "#FFFFFF"
+  },
+  fullScreenOverlay: {
+    width: "100%", // Full width
+    height: "100%", // Full height
+    marginTop: 0, // No margin
+    padding: 0 // No padding
+  }
+});
+
+export default EditUserAddressOverlay;
