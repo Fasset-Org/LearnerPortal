dec7f58bb60a1597bbc80d8691f99506929e1aec
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-09-10 20:57:41 +0200

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,84 +1,142 @@
 import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 import React from "react";
 import { Card, Icon } from "@rneui/themed";
-import { CardTitle } from "@rneui/base/dist/Card/Card.Title";
 import themeLight from "../../Theme";
+import EditUserAddressOverlay from "../../components/EditUserAddressOverlay";
+import EditUserInfoOverlay from "../../components/EditUserInfoOverlay";
 
 const TabRootLayout = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
-        <Card>
+        <Card containerStyle={styles.cardContainer}>
           <View style={styles.cardHeader}>
-            <Text style={styles.cardHeaderText}>Personal Information</Text>
+            <Text style={[styles.cardHeaderText, styles.wrapText]}>
+              Personal Information
+            </Text>
             <View style={styles.iconContainer}>
-              <Icon
+              {/* <Icon
                 name="pencil-square-o"
-                size={30}
+                size={20}
                 type="font-awesome"
                 style={styles.iconStyle}
                 color="#FFFFFF"
-              />
+              /> */}
+              <EditUserInfoOverlay />
             </View>
           </View>
 
           <View style={styles.cardDetail}>
-            <View style={styles.cardDetailAvatar}>
+            {/* <View style={styles.cardDetailAvatar}>
               <Text style={styles.cardDetailAvatarTitle}>TM</Text>
-            </View>
-            <Text>Tiyisela Themba Makamu</Text>
+            </View> */}
             <View style={styles.row}>
-              <Text style={styles.boldText}>ID Number:</Text>
-              <Text>9804046210080</Text>
-            </View>
-            <View style={styles.row}>
-              <Text style={styles.boldText}>Email:</Text>
-              <Text>kamzen1994@gmail.com</Text>
+              <Text style={styles.boldText}>
+                Full Name
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+              </Text>
+              <Text style={styles.wrapText}>Tiyisela Themba Makamu</Text>
             </View>
 
             <View style={styles.row}>
-              <Text style={styles.boldText}>Contact:</Text>
-              <Text>+27 797126016</Text>
+              <Text style={styles.boldText}>
+                ID Number
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+              </Text>
+              <Text style={styles.wrapText}>9804046210080</Text>
+            </View>
+            <View style={styles.row}>
+              <Text style={styles.boldText}>
+                Email
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+              </Text>
+              <Text style={styles.wrapText}>kamzen1994@gmail.com</Text>
             </View>
 
             <View style={styles.row}>
-              <Text style={styles.boldText}>Occupation:</Text>
-              <Text>I obtained a matric certificate</Text>
+              <Text style={styles.boldText}>
+                Contact
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+              </Text>
+              <Text style={styles.wrapText}>+27 797126016</Text>
+            </View>
+
+            <View style={styles.row}>
+              <Text style={styles.boldText}>
+                Occupation
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+              </Text>
+              <Text style={styles.wrapText}>
+                I obtained a matric certificate
+              </Text>
             </View>
           </View>
-          <Card.Divider />
+        </Card>
+        <Card containerStyle={styles.cardContainer}>
           <View style={styles.cardHeader}>
             <View>
-              <Text style={styles.cardHeaderText}>Address Information</Text>
+              <Text style={[styles.cardHeaderText, styles.wrapText]}>
+                Address Information
+              </Text>
             </View>
             <View style={styles.iconContainer}>
-              <Icon
+              {/* <Icon
                 name="pencil-square-o"
-                size={30}
+                size={20}
                 type="font-awesome"
                 style={styles.iconStyle}
                 color="#FFFFFF"
-              />
+              /> */}
+              <EditUserAddressOverlay />
             </View>
           </View>
 
           <View style={styles.cardDetail}>
-            <Text>1386, Mthimunye Street</Text>
             <View style={styles.row}>
-              <Text>Ga-Rankuwa Unit 23</Text>
-            </View>
-            <View style={styles.row}>
-              <Text>Ga-Rankuwa</Text>
-            </View>
-            <View style={styles.row}>
-              <Text>Gauteng</Text>
-            </View>
-            <View style={styles.row}>
-              <Text>0208</Text>
+              <Text style={styles.boldText}>Street Number/Name &nbsp;:</Text>
+              <Text style={styles.wrapText}>1386, Mthimunye Street</Text>
             </View>
 
             <View style={styles.row}>
-              <Text>City of Tshwane Metropolitan Municipality</Text>
+              <Text style={styles.boldText}>
+                Suburb/Town
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+              </Text>
+              <Text style={styles.wrapText}>Ga-Rankuwa Unit 23</Text>
+            </View>
+
+            <View style={styles.row}>
+              <Text style={styles.boldText}>
+                City
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+              </Text>
+              <Text style={styles.wrapText}>Ga-Rankuwa</Text>
+            </View>
+
+            <View style={styles.row}>
+              <Text style={styles.boldText}>
+                Province
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+              </Text>
+              <Text style={styles.wrapText}>Gauteng</Text>
+            </View>
+
+            <View style={styles.row}>
+              <Text style={styles.boldText}>
+                Postal Code
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+              </Text>
+              <Text style={styles.wrapText}>0208</Text>
+            </View>
+
+            <View style={styles.row}>
+              <Text style={styles.boldText}>
+                Manicipality
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+              </Text>
+              <Text style={styles.wrapText}>
+                City of Tshwane Metropolitan Municipality
+              </Text>
             </View>
 
             <Text style={styles.dangerText}>
@@ -98,13 +156,17 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#FFFFFF",
     flex: 1,
-    rowGap: 10
+    rowGap: 10,
+    padding: 10
+  },
+  cardContainer: {
+    borderRadius: 10,
+    margin: 0
   },
   cardHeader: {
     // borderWidth: 1,
     display: "flex",
     flexDirection: "row",
-    padding: 2,
     justifyContent: "space-between",
     alignItems: "center"
   },
@@ -114,13 +176,9 @@ const styles = StyleSheet.create({
     color: themeLight.lightColors?.primary
   },
   cardDetail: {
-    // borderWidth: 1,
-    marginTop: 10,
-    padding: 10,
+    width: "100%",
     display: "flex",
-    // flexDirection: "row",
     justifyContent: "center",
-    alignItems: "center",
     rowGap: 10
   },
   cardDetailAvatar: {
@@ -149,9 +207,9 @@ const styles = StyleSheet.create({
     color: themeLight.lightColors?.error
   },
   iconContainer: {
-    height: 60,
-    width: 60,
-    borderRadius: 30,
+    height: 40,
+    width: 40,
+    borderRadius: 20,
     borderWidth: 1,
     borderColor: "lightgray",
     display: "flex",
@@ -161,5 +219,8 @@ const styles = StyleSheet.create({
   },
   iconStyle: {
     color: "#FFFFFF"
+  },
+  wrapText: {
+    flexShrink: 1
   }
 });
