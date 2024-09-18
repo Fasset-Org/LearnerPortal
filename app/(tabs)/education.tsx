e5b868f7e8c1c413c67e17369f7ecf0246507d03
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-09-18 09:54:06 +0200

    Update education.tsx

--- app/(tabs)/education.tsx
+++ app/(tabs)/education.tsx
@@ -2,6 +2,7 @@ import { View, Text, StyleSheet, SafeAreaView, FlatList } from "react-native";
 import React from "react";
 import { Card, Icon } from "@rneui/themed";
 import themeLight from "../../Theme";
+import AddEditBasicEducationOverlay from "../../components/AddEditBasicEducationOverlay";
 
 const Education = () => {
   const list: any = [
@@ -38,62 +39,70 @@ const Education = () => {
       <Card containerStyle={styles.cardContainer}>
         <View style={styles.cardHeader}>
           <Icon
-            name="graduation-cap"
+            name="book-open"
+            type="font-awesome-5"
             size={30}
-            color="#000"
-            type="font-awesome"
+            color={themeLight.lightColors?.primary}
           />
           <Text style={[styles.cardHeaderText, styles.wrapText]}>
             Basic Education
           </Text>
         </View>
 
-        <FlatList
-          data={list}
-          renderItem={({ item }) => {
-            return (
-              <View style={styles.listItem}>
-                <Text style={styles.bullet}>{"\u2022"}</Text>
-                <Text style={styles.itemText}>
-                  {item.school} {" - "} {"Grade " + item.grade}
-                  {" - "} {item.city}
-                  {" - "} {item.province}
-                </Text>
-              </View>
-            );
-          }}
-          // keyExtractor={(item) => item}
-        />
+        <View style={styles.cardHead}>
+          <FlatList
+            data={list}
+            renderItem={({ item }) => {
+              return (
+                <View style={styles.listItem}>
+                  <Text style={styles.bullet}>{"\u2022"}</Text>
+                  <Text style={styles.itemText}>
+                    {item.school} {" - "} {"Grade " + item.grade}
+                    {" - "} {item.city}
+                    {" - "} {item.province}
+                  </Text>
+                </View>
+              );
+            }}
+            // keyExtractor={(item) => item}
+          />
+          <AddEditBasicEducationOverlay />
+        </View>
       </Card>
 
       {/* Tertiary Education */}
 
       <Card containerStyle={styles.cardContainer}>
-        <View style={styles.cardHeader}>
-          <Icon
-            name="graduation-cap"
-            size={30}
-            color="#000"
-            type="font-awesome"
-          />
-          <Text style={[styles.cardHeaderText, styles.wrapText]}>
-            Tertiary Education
-          </Text>
+        <View style={styles.cardHead}>
+          <View style={styles.cardHeader}>
+            <Icon
+              name="graduation-cap"
+              size={30}
+              color={themeLight.lightColors?.primary}
+              type="font-awesome"
+            />
+            <Text style={[styles.cardHeaderText, styles.wrapText]}>
+              Tertiary Education
+            </Text>
+          </View>
         </View>
 
         <FlatList
           data={tertiaryEducationList}
           renderItem={({ item }) => {
             return (
-              <View style={styles.listItem}>
-                <Text style={styles.bullet}>{"\u2022"}</Text>
-                <Text style={styles.itemText}>
-                  {item.institution} {"\u2022"} {item.educationLevel}
-                  {" in "} {item.fieldOfStufy}
-                  {"  \u2022"} {item.startYear}
-                  {" - "} {item.endYear}
-                  {"  \u2022"} {item.status}
-                </Text>
+              <View style={styles.cardHead}>
+                <View style={styles.listItem}>
+                  <Text style={styles.bullet}>{"\u2022"}</Text>
+                  <Text style={styles.itemText}>
+                    {item.institution} {"\u2022"} {item.educationLevel}
+                    {" in "} {item.fieldOfStufy}
+                    {"  \u2022"} {item.startYear}
+                    {" - "} {item.endYear}
+                    {"  \u2022"} {item.status}
+                  </Text>
+                </View>
+                <AddEditBasicEducationOverlay />
               </View>
             );
           }}
@@ -195,14 +204,31 @@ const styles = StyleSheet.create({
 
   cardContainer: {
     borderWidth: 1,
-    borderColor: "lightgray",
-    shadowColor: "lightgray",
-    borderRadius: 10
+    borderColor: themeLight.lightColors?.primary,
+    borderRadius: 10,
+
+    // iOS Shadow
+    shadowColor: "#000", // Shadow color
+    shadowOffset: { width: 0, height: 4 }, // Shadow offset
+    shadowOpacity: 0.3, // Shadow opacity
+    shadowRadius: 4.65, // Shadow blur radius
+
+    // Android Elevation
+    elevation: 8 // Elevation for Android shadow
+  },
+  cardHead: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10
+    // borderWidth: 1
   },
   cardHeader: {
     display: "flex",
     flexDirection: "row",
     columnGap: 10
+    // borderWidth: 1
   },
   wrapText: {
     flexShrink: 1
