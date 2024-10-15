1e0a06d1ac5f2e2a1cf741de3008139ce30caf08
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-10-15 22:40:32 +0200

    Update AuthContext.tsx

--- components/AuthContext.tsx
+++ components/AuthContext.tsx
@@ -3,6 +3,8 @@ import React, { createContext, ReactNode, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import StudentQuery from "../app/xhr/student";
 import SplashScreen from "./SplashScreen";
+import ErrorComponent from "./ErrorComponent";
+import { AxiosError } from "axios";
 
 const AuthContext = createContext<unknown>(null);
 
@@ -13,7 +15,7 @@ interface Props {
 const AuthProvider = ({ children }: Props) => {
   const [isAuth, setIsAuth] = useState<boolean>();
 
-  const { data, error, isPending } = useQuery({
+  const { data, error, isPending, isError } = useQuery({
     queryKey: ["userInfo"],
     queryFn: () => StudentQuery.getUserInfo()
   });
@@ -22,6 +24,15 @@ const AuthProvider = ({ children }: Props) => {
 
   if (data) {
     setIsAuth(true);
+  }
+
+  if (
+    isError &&
+    ((error as AxiosError).response.status !== 401 ||
+      (error as AxiosError).response.status !== 500)
+  ) {
+    console.log((error as AxiosError).response);
+    return <ErrorComponent />;
   }
 
   if (isPending) {
