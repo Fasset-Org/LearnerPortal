12221c800f687505a9f3c143b1b662b3b55d6f5c
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-10-15 22:13:05 +0200

    Updated auth provider to render children components

--- components/AuthContext.tsx
+++ components/AuthContext.tsx
@@ -1,6 +1,36 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { createContext, ReactNode, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import StudentQuery from "../app/xhr/student";
+import SplashScreen from "./SplashScreen";
 
-export default function AuthContext() {
-  return <View></View>;
+const AuthContext = createContext<unknown>(null);
+
+interface Props {
+  children: ReactNode;
 }
+
+const AuthProvider = ({ children }: Props) => {
+  const [isAuth, setIsAuth] = useState<boolean>();
+
+  const { data, error, isPending } = useQuery({
+    queryKey: ["userInfo"],
+    queryFn: () => StudentQuery.getUserInfo()
+  });
+
+  console.log(data as any);
+
+  if (data) {
+    setIsAuth(true);
+  }
+
+  if (isPending) {
+    return <SplashScreen loading={isPending} />;
+  }
+
+  return (
+    <AuthContext.Provider value={{ isAuth }}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
