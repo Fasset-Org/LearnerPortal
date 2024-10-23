67fe02ba788489861721c04156df3317804d9ca6
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-10-23 12:58:30 +0200

    Update student.ts

--- app/xhr/student.ts
+++ app/xhr/student.ts
@@ -5,6 +5,12 @@ const StudentQuery = {
     const resp = await axiosIntance.get(`/auth/isUserLoggedIn`);
 
     return resp?.data;
+  },
+
+  addAddresInfo: async (formData: any) => {
+    const resp = await axiosIntance.get(`/student/addAddress`, formData);
+
+    return resp?.data;
   }
 };
 
