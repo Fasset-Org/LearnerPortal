50de6c9ce05cd1f8095fc38baac12e7ec75a0fbd
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-10-24 13:06:29 +0200

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,57 +1,44 @@
-import { AppState, AppStateStatus, Platform, StyleSheet } from "react-native";
-import React, { useEffect } from "react";
+import { StyleSheet } from "react-native";
+import React from "react";
 import { Stack } from "expo-router";
 import CustomHeaderTitle from "../components/CustomHeaderTitle";
 import { ThemeProvider } from "@rneui/themed";
 import themeLight from "../Theme";
-import {
-  focusManager,
-  QueryClient,
-  QueryClientProvider
-} from "@tanstack/react-query";
-
-function onAppStateChange(status: AppStateStatus) {
-  if (Platform.OS !== "web") {
-    focusManager.setFocused(status === "active");
-  }
-}
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "../components/AuthContext";
 
 const RootLayout = () => {
   const queryClient = new QueryClient();
 
-  useEffect(() => {
-    const subscription = AppState.addEventListener("change", onAppStateChange);
-
-    return () => subscription.remove();
-  }, []);
-
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={themeLight}>
-        <Stack>
-          <Stack.Screen
-            name="index"
-            options={{
-              headerStyle: {
-                backgroundColor: themeLight.lightColors?.primary
-              },
-              headerTitleStyle: { color: "#FFFFFF" },
-              headerTitle: () => <CustomHeaderTitle />
-            }}
-          />
-          <Stack.Screen
-            name="(tabs)"
-            options={{
-              headerShown: false
-            }}
-          />
-          <Stack.Screen
-            name="(auth)"
-            options={{
-              headerShown: false
-            }}
-          />
-        </Stack>
+        <AuthProvider>
+          <Stack>
+            <Stack.Screen
+              name="index"
+              options={{
+                headerStyle: {
+                  backgroundColor: themeLight.lightColors?.primary
+                },
+                headerTitleStyle: { color: "#FFFFFF" },
+                headerTitle: () => <CustomHeaderTitle />
+              }}
+            />
+            <Stack.Screen
+              name="(tabs)"
+              options={{
+                headerShown: false
+              }}
+            />
+            <Stack.Screen
+              name="(auth)"
+              options={{
+                headerShown: false
+              }}
+            />
+          </Stack>
+        </AuthProvider>
       </ThemeProvider>
     </QueryClientProvider>
   );
