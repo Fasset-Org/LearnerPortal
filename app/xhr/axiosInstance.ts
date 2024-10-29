dd5a9cf120ca8a7f0fb05ce8ca67f964f987501c
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-10-29 08:19:59 +0200

    Update axiosInstance.ts

--- app/xhr/axiosInstance.ts
+++ app/xhr/axiosInstance.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
-const BASE_API_URL: string = "http://172.16.248.155:8000/api/v1";
+const BASE_API_URL: string = "http://192.168.0.112:8000/api/v1";
 import * as SecureStore from "expo-secure-store";
 
 const token = SecureStore.getItem("userToken");
-
-console.log("token", token);
 
 const axiosInstance = axios.create({
   baseURL: BASE_API_URL,
