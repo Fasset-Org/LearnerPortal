c73bef3af0bec95d3b4cddf49ce73d4ab4c1204a
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-10-29 18:28:00 +0200

    Create AddEditProfessionalSkillOverlay.tsx

---
+++ components/AddEditProfessionalSkillOverlay.tsx
@@ -0,0 +1,196 @@
+import React, { useState } from "react";
+import { Button, Overlay, Icon, useTheme } from "@rneui/themed";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ActivityIndicator
+} from "react-native";
+import themeLight from "../Theme";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import TextInputWrapper from "./FormComponents/TextInputWrapper";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import StudentQuery from "../app/xhr/student";
+import { showToast } from "../utils/showToast";
+import SelectInputWrapper from "./FormComponents/SelectInputWrapper";
+import Toast from "react-native-toast-message";
+
+type AddEditProfessionalSkillOverlay = {
+  education?: any;
+  userId?: string | any;
+};
+
+const AddEditProfessionalSkillOverlay: React.FunctionComponent<
+  AddEditProfessionalSkillOverlay
+> = ({ education, userId }) => {
+  const { theme } = useTheme();
+  const [visible, setVisible] = useState(false);
+  const queryClient: any = useQueryClient();
+
+  console.log(education);
+
+  const addProfessionalSkillMutation = useMutation({
+    mutationFn: (formData: any) => StudentQuery.addProfessionalSkill(formData),
+    onSuccess: (data: any) => {
+      showToast("success", "Success", data?.message);
+      queryClient.invalidateQueries(["userInfo"]);
+    },
+    onError: (err: any) => {
+      showToast("error", "Error", err?.response?.data?.message);
+    }
+  });
+
+  const toggleOverlay = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <View>
+      {education ? (
+        <Icon
+          name="pencil-square-o"
+          size={20}
+          type="font-awesome"
+          // style={styles.iconStyle}
+          color={themeLight.lightColors?.primary}
+          onPress={toggleOverlay}
+        />
+      ) : (
+        <Icon
+          name="add"
+          size={20}
+          type="material"
+          // style={styles.iconStyle}
+          color={themeLight.lightColors?.white}
+          onPress={toggleOverlay}
+        />
+      )}
+      <Overlay
+        isVisible={visible}
+        onBackdropPress={toggleOverlay}
+        overlayStyle={styles.fullScreenOverlay}
+      >
+        <View style={styles.overlayHeader}>
+          <Text style={styles.textPrimary}>
+            {education ? "Edit Professional Skill" : "Add Professional Skill"}
+          </Text>
+          <Icon
+            name="close"
+            type="material"
+            color="white"
+            size={30}
+            onPress={toggleOverlay}
+          />
+        </View>
+        <View style={{ padding: 10 }}>
+          <Formik
+            initialValues={{
+              userId: userId || "",
+              skill: "",
+              skillLevel: ""
+            }}
+            validationSchema={Yup.object().shape({
+              skill: Yup.string().required("Please enter skill"),
+              skillLevel: Yup.string().required("Please select skill level")
+            })}
+            enableReinitialize={true}
+            onSubmit={(values) => {
+              addProfessionalSkillMutation.mutate(values);
+            }}
+          >
+            {({ handleSubmit, setFieldValue, values }) => (
+              <View style={styles.innerContainer}>
+                <TextInputWrapper
+                  name="skill"
+                  label="Skill"
+                  secureTextEntry={false}
+                />
+                <SelectInputWrapper
+                  name="skillLevel"
+                  label="Skill Level"
+                  options={[
+                    {
+                      value: "Beginner",
+                      label: "Beginner"
+                    },
+                    {
+                      value: "Intermediate",
+                      label: "Intermediate"
+                    },
+                    {
+                      value: "Expert",
+                      label: "Expert"
+                    }
+                  ]}
+                />
+
+                {addProfessionalSkillMutation.isPending ? (
+                  <ActivityIndicator size="large" animating={true} />
+                ) : (
+                  <TouchableOpacity style={styles.button}>
+                    <Button
+                      title="SAVE"
+                      type="outline"
+                      buttonStyle={{
+                        borderColor: theme.colors.secondary,
+                        borderWidth: 1
+                      }}
+                      titleStyle={{
+                        color: theme.colors.secondary,
+                        marginRight: 10
+                      }}
+                      onPress={() => handleSubmit()}
+                    />
+                  </TouchableOpacity>
+                )}
+              </View>
+            )}
+          </Formik>
+        </View>
+        <Toast />
+      </Overlay>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    margin: 10
+  },
+  overlayHeader: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderColor: "lightgray",
+    backgroundColor: themeLight.lightColors?.primary,
+    paddingHorizontal: 10,
+    height: 60
+  },
+  textPrimary: {
+    fontSize: 17,
+    color: "#FFFFFF"
+  },
+  textSecondary: {
+    marginBottom: 10,
+    textAlign: "center",
+    fontSize: 17
+  },
+  iconStyle: {
+    color: "#FFFFFF"
+  },
+  fullScreenOverlay: {
+    width: "100%", // Full width
+    height: "100%", // Full height
+    marginTop: 0, // No margin
+    padding: 0 // No padding
+  },
+  innerContainer: {
+    rowGap: 0
+  }
+});
+
+export default AddEditProfessionalSkillOverlay;
