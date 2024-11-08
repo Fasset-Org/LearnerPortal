19391615d1882906a0d415238dc6091effc012f4
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-11-08 11:23:38 +0200

    Update axiosInstance.ts

--- app/xhr/axiosInstance.ts
+++ app/xhr/axiosInstance.ts
@@ -1,15 +1,31 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
-const BASE_API_URL: string = "http://172.16.248.178:8000/api/v1";
+const BASE_API_URL: string = "http://192.168.0.112:8000/api/v1";
 import * as SecureStore from "expo-secure-store";
 
-const token = SecureStore.getItem("userToken");
+// const token = SecureStore.getItem("userToken");
+const getToken = async () => {
+  return await AsyncStorage.getItem("token");
+};
 
 const axiosInstance = axios.create({
   baseURL: BASE_API_URL,
   headers: {
-    Authorization: `Bearer ${token}`,
     "Content-Type": "application/json"
   }
 });
+
+axiosInstance.interceptors.request.use(
+  async (config: any) => {
+    const token = await getToken();
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
