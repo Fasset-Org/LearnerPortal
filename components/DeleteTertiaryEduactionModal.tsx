d9d6b29ae6255579a1bbd38b5f13881544eb1c42
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-11-11 21:22:51 +0200

    Create DeleteTertiaryEduactionModal.tsx

---
+++ components/DeleteTertiaryEduactionModal.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Dialog } from "@rneui/themed";
+import { Icon } from "@rneui/base";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import StudentQuery from "../app/xhr/student";
+import { showToast } from "../utils/showToast";
+import Toast from "react-native-toast-message";
+import { ActivityIndicator } from "react-native-paper";
+
+const DeleteTertiaryEducationModal = ({ id }: { id: string }) => {
+  const [visible, setVisible] = useState(false);
+
+  const queryClient: any = useQueryClient();
+
+  const deleteMutatuon = useMutation({
+    mutationFn: (id: string) => StudentQuery.deleteTertiaryEducation(id),
+    onSuccess: (data: any) => {
+      showToast("success", "Success", data?.message);
+      queryClient.invalidateQueries(["userInfo"]);
+      setVisible(false);
+    },
+    onError: (err: any) => {
+      showToast("error", "Error", err?.response?.data?.message);
+    }
+  });
+
+  const toggleModal = () => {
+    setVisible(!visible);
+  };
+
+  const handleDelete = (id: string) => {
+    deleteMutatuon.mutate(id);
+  };
+
+  return (
+    <>
+      <TouchableOpacity onPress={toggleModal}>
+        <View
+          style={{
+            height: 30,
+            width: 30,
+            borderRadius: 20,
+            borderWidth: 1,
+            borderColor: "lightgray",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "lightgray"
+          }}
+        >
+          <Icon
+            name="delete"
+            type="material"
+            size={20}
+            color="red"
+
+            // style={{ borderWidth: 1, width: "fit-content" }}
+          />
+        </View>
+      </TouchableOpacity>
+
+      {/* <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          borderWidth: 1
+        }}
+      > */}
+      <Dialog isVisible={visible} onBackdropPress={toggleModal}>
+        <Dialog.Title title="Confirm Delete" />
+        <Text>Are you sure you want to delete this item?</Text>
+        <Dialog.Actions>
+          <Dialog.Button title="Cancel" onPress={toggleModal} />
+          {deleteMutatuon.isPending ? (
+            <ActivityIndicator animating={true} />
+          ) : (
+            <Dialog.Button
+              title="Delete"
+              onPress={() => handleDelete(id)}
+              color="red"
+            />
+          )}
+        </Dialog.Actions>
+      </Dialog>
+      {/* </View> */}
+    </>
+  );
+};
+
+export default DeleteTertiaryEducationModal;
