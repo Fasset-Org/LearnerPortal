1057d08780c6a6e821ff3678cfa02b20918800f8
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-11-18 16:23:19 +0200

    Update _layout.tsx

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,30 +1,56 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import themeLight from "../../Theme";
+import { Icon } from "@rneui/themed";
 
 const RootLayout = () => {
+  const router = useRouter();
   return (
-    <Stack>
-      <Stack.Screen
-        name="login"
-        options={{
-          headerStyle: { backgroundColor: themeLight.lightColors?.primary },
-          headerTitleStyle: { color: "#FFFFFF" },
-          headerTitle: "Login",
-          headerTintColor: "#FFFFFF"
-        }}
-      />
-      <Stack.Screen
-        name="register"
-        options={{
-          headerStyle: { backgroundColor: themeLight.lightColors?.primary },
-          headerTitleStyle: { color: "#FFFFFF" },
-          headerTitle: "Register",
-          headerTintColor: "#FFFFFF"
-        }}
-      />
-    </Stack>
+    <View style={{ flex: 1 }}>
+      <Stack>
+        <Stack.Screen
+          name="login"
+          options={{
+            headerStyle: { backgroundColor: themeLight.lightColors?.primary },
+            headerTitleStyle: { color: "#FFFFFF" },
+            headerTitle: "Login",
+            headerTintColor: "#FFFFFF",
+            headerTitleAlign: "left",
+            headerLeft: () => (
+              <Icon
+                name="arrow-back"
+                type="material"
+                color="#fff"
+                onPress={() => router.back()} // Go back to the previous screen
+                size={30}
+                containerStyle={{ marginRight: 5 }}
+              />
+            )
+          }}
+        />
+        <Stack.Screen
+          name="register"
+          options={{
+            headerStyle: { backgroundColor: themeLight.lightColors?.primary },
+            headerTitleStyle: { color: "#FFFFFF" },
+            headerTitle: "Register",
+            headerTintColor: "#FFFFFF",
+            headerTitleAlign: "left",
+            headerLeft: () => (
+              <Icon
+                name="arrow-back"
+                type="material"
+                color="#fff"
+                onPress={() => router.back()} // Go back to the previous screen
+                size={30}
+                containerStyle={{ marginRight: 5 }}
+              />
+            )
+          }}
+        />
+      </Stack>
+    </View>
   );
 };
 
