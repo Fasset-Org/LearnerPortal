dbdd7cb58c105b411390c6f7225604da6a99d02f
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-11-18 16:23:15 +0200

    Update TextInputWrapper.tsx

--- components/FormComponents/TextInputWrapper.tsx
+++ components/FormComponents/TextInputWrapper.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet } from "react-native";
 import React from "react";
 import { useField } from "formik";
-import { Input } from "@rneui/base";
+import { Input } from "@rneui/themed";
 import { useTheme } from "@rneui/themed";
 
 type PropType = {
@@ -33,13 +33,17 @@ const TextInputWrapper = ({
       style={styles.input}
       inputContainerStyle={{
         borderBottomWidth: 0,
-        marginTop: 5
+        // borderWidth: 1,
+
+        margin: 0,
+        padding: 0
+        // borderColor: "lightgray"
       }}
       disabled={disable || false}
-      labelStyle={{ fontSize: 13 }}
+      labelStyle={{ fontSize: 13, marginBottom: 5, marginTop: 0, padding: 0 }}
       placeholderTextColor={"#aeb6bf"}
-      inputStyle={{ fontSize: 13, fontWeight: "bold" }}
-      containerStyle={{ margin: 0 }}
+      inputStyle={{ fontSize: 13, fontWeight: "bold", margin: 0, padding: 0 }}
+      containerStyle={{ margin: 0, padding: 0, height: 60, marginBottom: 30 }}
     />
   );
 };
@@ -49,7 +53,9 @@ export default TextInputWrapper;
 const styles = StyleSheet.create({
   input: {
     borderWidth: 1,
-    padding: 10,
+    // padding: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 12,
     borderColor: `#d6dbdf`,
     borderRadius: 5,
     width: "100%"
