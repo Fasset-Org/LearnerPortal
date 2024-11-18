a96636f193806d9a07de0e8bce7f1682c88253d2
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-11-18 16:23:29 +0200

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, TouchableOpacity } from "react-native";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import { Icon, useTheme } from "@rneui/themed";
@@ -9,88 +9,102 @@ const DashBoardTabs = () => {
   const { theme } = useTheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarStyle: { paddingBottom: 5 },
-        tabBarLabelStyle: {
-          color: theme.colors.primary,
-          fontWeight: "bold",
-          textAlign: "center"
-        },
-
-        tabBarIconStyle: {
-          color: theme.colors.primary,
-          backgroundColor: theme.colors.primary
-        },
-        headerStyle: {
-          borderWidth: 3
-        },
-
-        headerTitleStyle: {
-          color: theme.colors.primary
-        },
-        tabBarInactiveTintColor: theme.colors.primary,
-
-        headerRight: () => <SignoutModal />
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          tabBarIcon: (colors) => {
-            return (
-              <Icon
-                name="user"
-                size={20}
-                type="font-awesome"
-                style={styles.icon}
-              />
-            );
-          },
-          title: "Profile",
-          headerTitle: "Profile",
-          headerStyle: {
-            borderBottomWidth: 1
-          }
-        }}
-      />
-
-      <Tabs.Screen
-        name="education"
-        options={{
-          tabBarIcon: (colors) => {
-            return <Icon name="graduation-cap" size={20} type="font-awesome" />;
-          },
-          title: "Education",
-          headerStyle: { borderBottomWidth: 1 }
-        }}
-      />
-
-      <Tabs.Screen
-        name="certification"
-        options={{
-          tabBarIcon: (colors) => {
-            return (
-              <Icon name="pencil-square-o" size={20} type="font-awesome" />
-            );
-          },
-          title: "Skills & Attachments",
-          headerStyle: { borderBottomWidth: 1 }
-        }}
-      />
-
-      <Tabs.Screen
-        name="learnerprogramme"
-        options={{
-          tabBarIcon: (colors) => {
-            return <Icon name="profile" size={20} type="antdesign" />;
-          },
-          title: "Programmes",
-          headerStyle: { borderBottomWidth: 1 },
-          headerTitle: "Learner Interventions"
-        }}
-      />
-    </Tabs>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <Tabs
+          screenOptions={{
+            tabBarStyle: {
+              borderWidth: 1,
+              borderColor: theme.colors.primary
+            },
+            tabBarLabelStyle: {
+              color: theme.colors.primary,
+              fontWeight: "bold",
+              textAlign: "center"
+            },
+            tabBarIconStyle: {
+              color: theme.colors.primary,
+              backgroundColor: theme.colors.primary
+            },
+            headerStyle: {
+              borderWidth: 3
+            },
+            headerTitleStyle: {
+              color: theme.colors.primary
+            },
+            tabBarInactiveTintColor: theme.colors.primary,
+            headerRight: () => <SignoutModal />
+          }}
+        >
+          <Tabs.Screen
+            name="index"
+            options={{
+              tabBarIcon: (colors) => (
+                <Icon
+                  name="user"
+                  size={20}
+                  type="font-awesome"
+                  color={theme.colors.primary}
+                />
+              ),
+              title: "Profile",
+              headerTitle: "Profile",
+              headerTitleAlign: "left",
+              headerStyle: { borderBottomWidth: 1 }
+            }}
+          />
+          <Tabs.Screen
+            name="education"
+            options={{
+              tabBarIcon: (colors) => (
+                <Icon
+                  name="graduation-cap"
+                  size={20}
+                  type="font-awesome"
+                  color={theme.colors.primary}
+                />
+              ),
+              title: "Education",
+              headerTitleAlign: "left",
+              headerStyle: { borderBottomWidth: 1 }
+            }}
+          />
+          <Tabs.Screen
+            name="certification"
+            options={{
+              tabBarIcon: (colors) => (
+                <Icon
+                  name="pencil-square-o"
+                  size={20}
+                  type="font-awesome"
+                  color={theme.colors.primary}
+                />
+              ),
+              title: "Skills & Attachments",
+              headerTitleAlign: "left",
+              headerStyle: { borderBottomWidth: 1 }
+            }}
+          />
+          <Tabs.Screen
+            name="learnerprogramme"
+            options={{
+              tabBarIcon: (colors) => (
+                <Icon
+                  name="profile"
+                  size={20}
+                  type="antdesign"
+                  color={theme.colors.primary}
+                />
+              ),
+              title: "Programmes",
+              headerTitleAlign: "left",
+              headerStyle: { borderBottomWidth: 1 },
+              headerTitle: "Learner Interventions"
+            }}
+          />
+        </Tabs>
+      </View>
+    </SafeAreaView>
   );
 };
 
