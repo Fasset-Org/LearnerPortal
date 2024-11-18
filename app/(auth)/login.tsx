77292d366e87764dee265afe0fcdab66dd67df01
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-11-18 16:23:22 +0200

    Update login.tsx

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -68,7 +68,7 @@ const Login = () => {
       router.replace(`/(tabs)`);
     },
     onError: (err: any) => {
-      console.log(err);
+      console.log(JSON.stringify(err as any));
       showToast("error", "Error", err?.response?.data?.message);
     }
   });
@@ -78,108 +78,112 @@ const Login = () => {
   }
 
   return (
-    <>
-      <ScrollView style={{ flex: 1, backgroundColor: "#FFFFFF" }}>
-        <View style={styles.container}>
-          <View style={styles.imageContainer}>
-            <Image
-              source={require(`../../assets/images/blueLogo-transparentBg.png`)}
-              style={styles.image}
-            />
-          </View>
-          <Text style={styles.title}>Welcome Back</Text>
-          <Text style={styles.subTitle}>
-            Login in to continue to Learner Portal
-          </Text>
-          <Formik
-            initialValues={{
-              email: "",
-              password: ""
-            }}
-            validationSchema={Yup.object({
-              email: Yup.string()
-                .email("Invalid email address")
-                .required("Email required"),
-              password: Yup.string()
-                .min(8, "Password must be at least 8 characters")
-                .required("Password required")
-            })}
-            onSubmit={async (values: FormData) => {
-              await queryClient.clear();
-              mutate(values);
-            }}
-            // Optionally add validationSchema here
-          >
-            {({ handleSubmit }) => (
-              <View style={styles.innerContainer}>
-                <TextInputWrapper
-                  name="email"
-                  label="Email"
-                  secureTextEntry={false}
-                />
-                <TextInputWrapper
-                  name="password"
-                  label="Password"
-                  secureTextEntry={true}
-                />
-                {isPending ? (
-                  <ActivityIndicator size="large" />
-                ) : (
-                  <TouchableOpacity style={styles.button}>
-                    <Button
-                      title="LOGIN"
-                      icon={<Icon name="login" size={20} color="#FFFFFF" />}
-                      iconPosition="right"
-                      color="primary"
-                      onPress={() => handleSubmit()}
-                    />
-                  </TouchableOpacity>
-                )}
+    <ScrollView
+      style={{
+        flex: 1,
+        backgroundColor: "#FFFFFF"
+      }}
+    >
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require(`../../assets/images/blueLogo-transparentBg.png`)}
+            style={styles.image}
+          />
+        </View>
+        <Text style={styles.title}>Welcome Back</Text>
+        <Text style={styles.subTitle}>
+          Login in to continue to Learner Portal
+        </Text>
+        <Formik
+          initialValues={{
+            email: "",
+            password: ""
+          }}
+          validationSchema={Yup.object({
+            email: Yup.string()
+              .email("Invalid email address")
+              .required("Email required"),
+            password: Yup.string()
+              .min(8, "Password must be at least 8 characters")
+              .required("Password required")
+          })}
+          onSubmit={async (values: FormData) => {
+            console.log("Mutation calling");
+            await queryClient.clear();
+            mutate(values);
+          }}
+          // Optionally add validationSchema here
+        >
+          {({ handleSubmit }) => (
+            <View style={styles.innerContainer}>
+              <TextInputWrapper
+                name="email"
+                label="Email"
+                secureTextEntry={false}
+              />
+              <TextInputWrapper
+                name="password"
+                label="Password"
+                secureTextEntry={true}
+              />
+              {isPending ? (
+                <ActivityIndicator size="large" />
+              ) : (
+                <TouchableOpacity style={styles.button}>
+                  <Button
+                    title="LOGIN"
+                    icon={<Icon name="login" size={20} color="#FFFFFF" />}
+                    iconPosition="right"
+                    color="primary"
+                    onPress={() => handleSubmit()}
+                  />
+                </TouchableOpacity>
+              )}
 
-                <View style={{ padding: 10 }}>
-                  <Text style={{ fontSize: 14, flexWrap: "nowrap" }}>
-                    By logging in and completing your profile, you agree that
-                    you have read, understood, and accepted that you will be
-                    bound by the terms of use of the
-                  </Text>
-                  <Text
-                    style={{
-                      fontSize: 14,
-                      color: "blue",
-                      textDecorationLine: "underline",
-                      flexWrap: "nowrap"
-                    }}
-                    onPress={() =>
-                      openURL(
-                        "https://www.learner-portal.fasset.org.za/static/media/FASSET%20POPIA%20POLICY.d44535ef675cac491a34.pdf"
-                      )
-                    }
-                  >
-                    POPI Act No.4 2013
-                  </Text>
-                  <Text style={{ fontSize: 14, flexWrap: "nowrap" }}>
-                    . FASSET endeavors to take all reasonable precautions to
-                    ensure that any information provided is only used for the
-                    purposes for which it has been provided.
-                  </Text>
-                </View>
-
-                <TouchableOpacity
+              <View style={{ padding: 10 }}>
+                <Text style={{ fontSize: 14, flexWrap: "nowrap" }}>
+                  By logging in and completing your profile, you agree that you
+                  have read, understood, and accepted that you will be bound by
+                  the terms of use of the
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    color: "blue",
+                    textDecorationLine: "underline",
+                    flexWrap: "nowrap"
+                  }}
                   onPress={() =>
                     openURL(
-                      "https://www.learner-portal.fasset.org.za/forgotPassword"
+                      "https://www.learner-portal.fasset.org.za/static/media/FASSET%20POPIA%20POLICY.d44535ef675cac491a34.pdf"
                     )
                   }
                 >
-                  <Text style={styles.forgotText}>Forgot Password?</Text>
-                </TouchableOpacity>
+                  POPI Act No.4 2013
+                </Text>
+                <Text style={{ fontSize: 14, flexWrap: "nowrap" }}>
+                  . FASSET endeavors to take all reasonable precautions to
+                  ensure that any information provided is only used for the
+                  purposes for which it has been provided.
+                </Text>
               </View>
-            )}
-          </Formik>
-        </View>
-      </ScrollView>
+
+              <TouchableOpacity
+                onPress={() =>
+                  openURL(
+                    "https://www.learner-portal.fasset.org.za/forgotPassword"
+                  )
+                }
+              >
+                <Text style={styles.forgotText}>Forgot Password?</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </Formik>
+      </View>
       <Toast />
-    </>
+    </ScrollView>
   );
 };
 
