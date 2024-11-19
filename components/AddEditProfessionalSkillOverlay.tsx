52479c08a136ade15bddd0166695e40fd7affb55
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-11-19 16:46:22 +0200

    Update AddEditProfessionalSkillOverlay.tsx

--- components/AddEditProfessionalSkillOverlay.tsx
+++ components/AddEditProfessionalSkillOverlay.tsx
@@ -36,9 +36,15 @@ const AddEditProfessionalSkillOverlay: React.FunctionComponent<
     onSuccess: (data: any) => {
       showToast("success", "Success", data?.message);
       queryClient.invalidateQueries(["userInfo"]);
+      setTimeout(() => {
+        setVisible(!visible);
+      }, 2000);
     },
     onError: (err: any) => {
       showToast("error", "Error", err?.response?.data?.message);
+      setTimeout(() => {
+        setVisible(!visible);
+      }, 2000);
     }
   });
 
