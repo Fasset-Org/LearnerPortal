b21217d8b19cb663a2815f015fb64a3beeef6e03
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-11-27 08:32:33 +0200

    Update auth.ts

--- app/xhr/auth.ts
+++ app/xhr/auth.ts
@@ -1,16 +1,24 @@
-import axios from "axios";
-import axiosIntance from "./axiosInstance";
+import axiosInstance from "./axiosInstance";
 
 const AuthQuery = {
   registerUser: async (formData: any) => {
-    const resp = await axiosIntance.post(`/auth/register`, formData);
+    const resp = await axiosInstance.post(`/auth/register`, formData);
     console.log(resp);
     return resp?.data;
   },
 
   loginUser: async (formData: any) => {
-    const resp = await axiosIntance.post(`/auth/login`, formData);
+    const resp = await axiosInstance.post(`/auth/login`, formData);
     console.log(resp);
+    return resp?.data;
+  },
+
+  deleteUser: async (formData: any) => {
+    const resp = await axiosInstance.put(
+      `/auth/deleteUser/${formData.userId}`,
+      formData
+    );
+
     return resp?.data;
   }
 };
